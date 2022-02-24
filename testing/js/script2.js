de7c3755e2237d3ffd0cb961f2c245e712a67371
NikPurvis <nikpurvis@gmail.com>
2022-02-23 20:24:48 -0700

    Woke items are back
    Array populating, items are rendering with random start position along the x-axis, and that start position is confined to the canvas.

--- testing/js/script2.js
+++ testing/js/script2.js
@@ -29,9 +29,9 @@ const randomNum = (min, max) => {
 // the caffeinated collectibles that the player will attempt
 // to capture.
 class WokeItem {
-    constructor(wokeName, wokeColor, wokeWidth, wokeHeight, wokeDropRate, wokeValue) {
-        this.x = randomNum(40, 770),
-        this.y = -50
+    constructor(wokeName, wokeColor, wokeWidth, wokeHeight, wokeDropRate, wokeValue, x) {
+        this.x = x,
+        this.y = 0,
         this.name = wokeName,
         this.color = wokeColor,
         this.width = wokeWidth,
@@ -42,7 +42,7 @@ class WokeItem {
 
         this.render = function() {
             ctx.fillStyle = this.color
-            ctx.fillRect(this.x, this.y, this.height, this.width)
+            ctx.fillRect(this.x, this.y, this.width, this.height)
         }
     }
 }
@@ -52,76 +52,74 @@ let gameOver = false
 // Starting value for the canvas level timer.
 let timer = 90
 
+
 const wokeItems = []
 const game = {
-
     makeWoke() {
-        const refill = new WokeItem("refill", "rgb(92, 47, 17)", 37, 28, 50, 10)
-        const energyDrink = new WokeItem("energyDrink", "silver", 25, 75, 100, 50)
-        const molecule = new WokeItem("molecule", "teal", 100, 75, 30, 100)
+        
+        
+
+        const refill = new WokeItem("refill", "rgb(92, 47, 17)", 28, 37, 50, 10, randomNum(0, canvas.width))
+        const energyDrink = new WokeItem("energyDrink", "silver", 25, 75, 100, 50, randomNum(0, canvas.width))
+        const molecule = new WokeItem("molecule", "teal", 100, 75, 30, 100, randomNum(0, canvas.width))
         
         let random = randomNum(1, 3)
         if (random === 1) {
+            if (refill.x > canvas.width - refill.width) {
+                refill.x = canvas.width - refill.width
+            }
             wokeItems.push(refill)
-            console.log("wokeItems @refill push:", wokeItems)
+            // console.log("made refill")
         } else if (random === 2) {
+            if (energyDrink.x > canvas.width - energyDrink.width) {
+                energyDrink.x = canvas.width - energyDrink.width
+            }
             wokeItems.push(energyDrink)
-            console.log("wokeItems @energyDrink push:", wokeItems)
+            // console.log("made nrg")
         } else if (random === 3) {
+            if (molecule.x > canvas.width - molecule.width) {
+                molecule.x = canvas.width - molecule.width
+            }
             wokeItems.push(molecule)
-            console.log("wokeItems @molecule push:", wokeItems)
+            // console.log("made molecule")
         }
-    }
+    },
+
+    dropWoke() {
+        for (let i = 0; i < wokeItems.length; i++) {
+            wokeItems[i].render()
+        }
+    },
+
+    // dropWoke() {
+    //     // for each element in the wokeitems array
+    //     // render that item into the gamefield
+    //     wokeItems.forEach(item =>
+    //         console.log(`${item.name} dropping!`))
+    //     // got to render item; new variable? the one I have for refill above isn't asking for x,y, so probably needs to be reworked
+    // },
 }
-    
+
 const gameLoop = () => {
     // console.log("This is the start of the game loop");
     // Clear the canvas
     clearCanvas()
-    
+    // will need a timer or some sort of checker to control how often this fires
+    game.dropWoke()
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    setInterval(gameLoop, 60)
-    // setInterval(game.makeWoke, 3000)
+const gameLoopInterval = setInterval(gameLoop, 60)
+const makeWokeInterval = setInterval(game.makeWoke, 1000)
+
+document.addEventListener("DOMContentLoaded", function () {    
+    // setInterval(gameLoop, 60)
+    setTimeout(stopGame, 5000)
 })
 
-// const game = {
-    
-//     startGame() {
-//         this.makeWoke();
-//         console.log("contents of wokeItems:", wokeItems)
-//     },
-
-//     makeWoke() {
-//         // Creation of the "woke" canvaspieces (to be replaced with
-//         // images) with the declared values: (wokeName, wokeColor,
-//         // wokeWidth, wokeHeight, wokeDropRate, wokeValue)
-//         if (gameOver === false) {
-//             const refill = new WokeItem("refill", "rgb(92, 47, 17)", 37, 28, 50, 10)
-//             const energyDrink = new WokeItem("energyDrink", "silver", 25, 75, 100, 50)
-//             const molecule = new WokeItem("molecule", "teal", 100, 75, 30, 100)
-//             // Create one of the items at random
-//             random = randomNum(1, 3)
-//             if (random === 1) {
-//                 this.wokeItems.push(refill)
-//             } else if (random === 2) {
-//                 this.wokeItems.push(energyDrink)
-//             } else if (random === 3) {
-//                 this.wokeItems.push(molecule)
-//             }
-//         }
-//     },
-// }   // game Object end bracket
-
-
-// const gameLoop = () => {
-//     // console.log("This is the start of the game loop");
-//     // Clear the canvas
-//     clearCanvas()
-//     // game.startGame()
-// }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     setInterval(gameLoop, 60)
-// })
+const stopGame = () => {
+    clearInterval(gameLoopInterval)
+    clearInterval(makeWokeInterval)
+    console.log("Game over!")
+    console.log("wokeitems generated:", wokeItems)
+    console.log("wokeitems[0]:", wokeItems[0].name)
+}
